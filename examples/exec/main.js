79051613162edc6b34ec4410afd2be31c65402b1
Spence Southard <Spence.Southard@gmail.com>
2017-09-21 06:54:50 -0400

    Removed debug code

--- examples/exec/main.js
+++ examples/exec/main.js
@@ -9,22 +9,3 @@ const badModel = {
 
 const result = validate(badModel, TestModel)
 console.log(result)
-
-{ 
-    model: 
-    { 
-        firstName: 'John',
-        lastName: 'Smith',
-        age: 'twenty seven',
-        favoriteFoods: [] 
-    },
-   errors: 
-    [ 
-        { 
-            property: 'age',
-            type: 'number',
-            message: 'Property age was of type string. Expected: number',
-            isRequired: true 
-        } 
-    ] 
-}
